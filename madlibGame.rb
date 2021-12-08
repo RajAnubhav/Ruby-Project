25f24446b8f000fea0b715b57256367efb9e9be3
RajAnubhav <anubhavraj742@gmail.com>
2021-12-08 07:10:43 +0530

    template game

---
+++ madlibGame.rb
@@ -0,0 +1,12 @@
+puts ("Enter a color :")
+color = gets.chomp()
+puts ("Enter a plural noun :")
+plural_noun = gets.chomp()
+puts ("Enter your favorite celebrity name :")
+name = gets.chomp()
+
+puts ()
+puts ("Roses are #{color}")
+puts ("#{plural_noun} is blue")
+puts ("I love #{name}")
+
